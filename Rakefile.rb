a605213a7317b39902e42de175c4a341eaefebfb
m4riusz <mariusz.sut.94+github@gmail.com>
2021-09-03 21:42:07 +0200

    add task for generating blank files

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,12 +1,32 @@
 
 namespace :project do
     desc "Generate project"
-    task :generate do
+    task :generate => [:generateAssetsFiles] do
         puts "Generating project file"
-        sh "mkdir -p JourneySpendings/Resources/Generated/"
-        sh "touch JourneySpendings/Resources/Generated/JourneyStrings.swift"
-        sh "touch JourneySpendings/Resources/Generated/JourneyImages.swift"
-        sh "rm -rf MobileApp.xcodeproj/"
+        sh "rm -rf JourneySpendings.xcodeproj/"
         sh "xcodegen generate --spec project.json"
+    end
+
+    desc "Generate assets files"
+    task :generateAssetsFiles do
+        puts "Generate assets files for About"
+        sh "mkdir -p About/Resources/Generated/"
+        sh "touch About/Resources/Generated/AboutStrings.swift"
+        sh "touch About/Resources/Generated/AboutImages.swift"
+
+        puts "Generate assets files for Core"
+        sh "mkdir -p Core/Resources/Generated/"
+        sh "touch Core/Resources/Generated/CoreStrings.swift"
+        sh "touch Core/Resources/Generated/CoreImages.swift"
+
+        puts "Generate assets files for Journey"
+        sh "mkdir -p Journey/Resources/Generated/"
+        sh "touch Journey/Resources/Generated/JourneyStrings.swift"
+        sh "touch Journey/Resources/Generated/JourneyImages.swift"
+
+        puts "Generate assets files for MobileApp"
+        sh "mkdir -p MobileApp/Resources/Generated/"
+        sh "touch MobileApp/Resources/Generated/MobileAppStrings.swift"
+        sh "touch MobileApp/Resources/Generated/MobileAppImages.swift"
     end
 end
